e4972ee9401237ea5c640f45b23fd92b39b64cb8
Sarina Li <74844953+sarinali@users.noreply.github.com>
2022-12-09 11:38:59 -0500

    added home assets

---
+++ src/ImageBundle.js
@@ -0,0 +1,9 @@
+class ImageBundle {
+    constructor(component, num, character) {
+        this.component = component;
+        this.index = num;
+        this.hanYuChar = character;
+     }
+}
+
+export default ImageBundle
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
+import First from '../assets/home_assets/1.JPG';
+import Second from '../assets/home_assets/2.JPG'
+import Third from '../assets/home_assets/3.JPG'
+import Fourth from '../assets/home_assets/4.JPG'
+import Fifth from '../assets/home_assets/5.JPG'
+import Sixth from '../assets/home_assets/6.JPG'
+import Seventh from '../assets/home_assets/7.JPG'
+import Eighth from '../assets/home_assets/8.JPG'
+import Ninth from '../assets/home_assets/9.JPG'
+import Tenth from '../assets/home_assets/10.JPG'
+import Eleventh from '../assets/home_assets/11.JPG'
+import ImageBundle from '../ImageBundle'
+
+
+
 function Home() {
+    let componentList = [
+        new ImageBundle(First, 1, '一'),
+        new ImageBundle(Second, 2, '二'),
+        new ImageBundle(Third, 3, '三'),
+        new ImageBundle(Fourth, 4, '四'),
+        new ImageBundle(Fifth, 5, '五'),
+        new ImageBundle(Sixth, 6, '六'),
+        new ImageBundle(Seventh, 7, '七'),
+        new ImageBundle(Eighth, 8, '八'),
+        new ImageBundle(Ninth, 9, '九'),
+        new ImageBundle(Tenth, 10, '十'),
+        new ImageBundle(Eleventh, 11, '十一'),
+    ];
+
+    let index = Math.floor(Math.random() * 11) + 1;
     return (
         <div>
-            home
+            {/* <img src={componentList[index].component}></img> */}
         </div>
     )
 }
