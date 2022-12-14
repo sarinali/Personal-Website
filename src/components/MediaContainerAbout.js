a5780e3be9a87859f1452969031d76a794d19fd4
Sarina Li <74844953+sarinali@users.noreply.github.com>
2022-12-14 00:49:40 -0500

    aboutme

--- src/components/MediaContainerAbout.js
+++ src/components/MediaContainerAbout.js
@@ -1,15 +1,14 @@
-function MediaContainerAbout({imageList, chineseCaption, caption, title}) {
-    const images = imageList.map((image) =>
-        <img src={image} alt='' className="image"></img>
-    );
+function MediaContainerAbout({imageList, chineseCaption, caption, title, itemIndex, itemTitle, descriptionList, link}) {
+    // const images = imageList.map((image) =>
+    //     <img src={image} alt='' className="image"></img>
+    // );
     return (
         <div className="media-body">
             <div className="image-media">
                 <div className="image-grid">
                     {imageList.map((image) => {
                         return (
-                            <img src={image} alt='' className="image"></img>
- 
+                            <img src={image} alt='' className="image-about"></img>
                         )
                     })}
 
@@ -18,7 +17,29 @@ function MediaContainerAbout({imageList, chineseCaption, caption, title}) {
                     <div className="caption">{caption}</div>
                     <div className="title">{title}</div>
             </div>
-            <div>alskdfjlkdj</div>
+           
+            <div className="about-text-container">
+                <div className="about-item-index">
+                    <div>{itemIndex}</div>
+                </div>
+                <div className="about-item-title">
+                    <div>{itemTitle}</div>
+                </div>
+                <div className="about-item-description">
+                    {descriptionList.map((text) => {
+                        return (
+                            <div className="about-text">
+                                <div>{text}</div>
+                            </div>
+                        )
+                    })}
+
+                    <a href={link} className='about-link'>
+                        <div>{link}</div>
+                    </a>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,10 +1,14 @@
 import MediaContainerAbout from "../components/MediaContainerAbout";
 import PageIntro from "../components/PageIntro";
 import '../css/aboutme.css'
-import Fashion1 from "../assets/aboutme_assets/fashion1.JPG"
-import Fashion2 from "../assets/aboutme_assets/fashion2.png"
-import Lifting from "../assets/aboutme_assets/lifting.jpg"
-
+import Fashion1 from "../assets/aboutme_assets/fashion1.JPG";
+import Fashion2 from "../assets/aboutme_assets/fashion2.png";
+import Lifting from "../assets/aboutme_assets/lifting.jpg";
+import Climbing from "../assets/aboutme_assets/climbing.jpg";
+import Nectar from "../assets/aboutme_assets/nectar.png";
+import Nuture from "../assets/aboutme_assets/nuture.jpeg";
+import OnTheSquare from "../assets/aboutme_assets/onthesquaree.jpeg";
+import Zephyr from "../assets/aboutme_assets/zephyr.png";
 
 function AboutMe() {
     return (
@@ -20,9 +24,11 @@ function AboutMe() {
         
             </div>
             <div className="media-container">
-                <MediaContainerAbout imageList={[Fashion2, Fashion1]} chineseCaption={'时尚。'} caption={'outfit making and makeup :)'} title={'Fashion'}></MediaContainerAbout>
+                <MediaContainerAbout imageList={[Fashion2, Fashion1]} chineseCaption={'时尚。'} caption={'outfit making and makeup :)'} title={'Fashion'} itemIndex={'| 00.'} itemTitle={'Fashion and Beauty'} descriptionList={['I am a little bit of a narcissist... I love looking good, dressing good and feeling good!', 'I love curating outfits, doing my makeup and online shopping. My favourite thing to do is browse the internet on platforms like Pinterest and 小红书 for the latest trends, outfit inspiration and makeup looks.', 'My philosophy is, look good, feel good!!']}></MediaContainerAbout>
+                <MediaContainerAbout imageList={[Lifting, Climbing]} chineseCaption={'锻炼。'} caption={'working out and bouldering 💪'} title={'Fitness'} itemIndex={'| 01.'} itemTitle={'Lifting and Bouldering'} descriptionList={['Fitness is a a stress reliever and a hobby for me!', 'Lately I have been visiting UofT’s Athletic Center to weight lift. I got into lifting a couple months ago and I have made it an official goal to one day squat 225 lbs, bench 135 lbs and deadlift 225lbs.', 'My I also used to boulder! The highest grade I climbed was a V4 :(. It was a fun past time until I had to give it up to save time for academics and extracurriculars.']}></MediaContainerAbout>
+                <MediaContainerAbout imageList={[Nuture, Nectar, OnTheSquare, Zephyr]} chineseCaption={'音乐。'} caption={"spotify's biggest fan"} title={'Music'} itemIndex={'| 02.'} itemTitle={'Music'} descriptionList={['As a former pianist and trumpeter, music is literally what keeps me going.', 'My headphones are glued to my head 24 hours a day, last year I clocked in 90,756 minutes of Spotify. My favourite artists are NIKI, Joji, Gorillaz and Porter Robinson. (I am a hopeless romantic)', 'You should add me on Spotify and recommend me some new tunes 😎']} link='https://open.spotify.com/user/sarinajin.li?si=44bf42485d004f05'></MediaContainerAbout>
+
             </div>
-            <div className="chinese-text"></div>
         </div>
         
     )
