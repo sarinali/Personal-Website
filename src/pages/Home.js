01db50b79bb9b896b172c01345f44e70e549e758
Sarina Li <74844953+sarinali@users.noreply.github.com>
2022-12-21 15:56:44 -0500

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,26 @@ function Home() {
     return (
         <div className="home-wrapper">
             <div className="home-introduction">
-                <div>aalsdkfjlasdkjf</div>
+            <div className="home-name-english-wrapper">
+                <div className="home-english-intro">My name is </div>
+                <div className="home-english-name">Sarina Li!</div>
+            </div>
+            <div className="home-name-chinese-wrapper">
+                <div className="home-chinese-intro">我叫</div>
+                <div className="home-chinese-name">李晶！</div>
+            </div>
+            <div class="wrapper">
+                <div className="home-words-intro">I am a</div>
+                <div class="words">
+                    <span className="individ-word">student</span>
+                    <span className="individ-word">developer</span>
+                    <span className="individ-word">lifter</span>
+                    <span className="individ-word">artist</span>
+                    <span className="individ-word">student</span>
+            </div>
+   </div>
+   <div className="home-description-intro">I am a 
+   <b className="bold-description"> student-learning-to-be-developer</b> with an interest in <b className="bold-description">creating</b>, <b className="bold-description">learning</b> and <b className="bold-description">building</b>! Creating: experiences and technology, Learning: anything and everything I can get my hands on, Building: technology and life experiences!</div>
             </div>
             <div className='home-body'>
                 <div className='media-wrapper'>
