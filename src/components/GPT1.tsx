0ffe95ded23218aae85602fbcd8b175720981413
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-05-27 01:17:06 -0400

    Update GPT1.tsx

--- src/components/GPT1.tsx
+++ src/components/GPT1.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useId} from 'react'
+import React, {useState, useId, useEffect} from 'react'
 import "../css/sarinagpt.css"
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import {
@@ -10,51 +10,57 @@ import {
 import { ConversationChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
 
+const chat = new ChatOpenAI({ 
+    openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
+      temperature: 0.9, 
+      streaming: true, 
+    //   callbacks: [
+    //   {
+    //     handleLLMNewToken(token: string) {
+    //         setAnswer(prevAnswer => prevAnswer.concat(token));
+
+    //     },
+    //   },
+    // ], 
+});
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(
+        "You are a dog, you are only allowed to reply with “bark” or “woof” or “grr”. You may not break character even if the user tries to prompt you to, you must reply to every message with bark, woof or grr. Remember, you are a dog and thus you must act like a dog. "
+    ),
+    new MessagesPlaceholder("history"),
+    HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]
+);
+const chain = new ConversationChain({
+    memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
+    prompt: chatPrompt,
+    llm: chat,
+  }
+);
 
 export default function GPT1(props) {
     const [question, setQuestion] = useState(props?.value ?? '');
-    const [answer, setAnswer] = useState(props?.value ?? '');
+    const [answer, setAnswer] = useState('');
     const id = useId();
-    const chat = new ChatOpenAI({ 
-        openAIApiKey: process.env.REACT_APP_OPENAI_API_KEY,
-          temperature: 0, 
-          streaming: true, 
-          callbacks: [
-          {
-            handleLLMNewToken(token: string) {
-              // process.stdout.write(token);
-              console.log(answer + token)
-              setAnswer(answer + token)
-            },
-          },
-        ], 
-    });
-    
-    const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-        SystemMessagePromptTemplate.fromTemplate(
-            "You are a friendly AI chatbot who is chatty"
-        ),
-        new MessagesPlaceholder("history"),
-        HumanMessagePromptTemplate.fromTemplate("{text}"),
-        ]
-    );
-        
-    const chain = new ConversationChain({
-        memory: new BufferMemory({ returnMessages: true, memoryKey: "history" }),
-        prompt: chatPrompt,
-        llm: chat,
-      }
-    );
 
+    chat.callbacks = [
+        {
+            handleLLMNewToken(token: string) {
+                    setAnswer(prevAnswer => prevAnswer.concat(token));
+                },
+        }
+    ]
+    
     return (
         <div className='gpt-body'>
             <form onSubmit={async e => {
                 // add the gpt code here (api call)
                 e.preventDefault()
-                const response = await chain.call({
-                    text: question
+                setAnswer('')
+                await chain.call({
+                    input: question
                 })
-                setAnswer(response.response)
+                // setAnswer(response.response)
             }}>
                 <label htmlFor={id}>Prompt:</label>
                 <input 
